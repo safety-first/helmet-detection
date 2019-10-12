23ed2f633570d064e5d265f5e4cd58c17479598a
safety-first <56191803+safety-first@users.noreply.github.com>
2019-10-12 13:00:45 +0300

    Update send.py

--- send.py
+++ send.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 import pika
-import zmq
-import random
-import sys
-import time
 import base64
 import cv2
 
@@ -20,18 +16,18 @@ success = 1
 camera = cv2.VideoCapture(0)  
 channel.queue_declare(queue='hello')
 while success: 
-		# vidObj object calls read 
+	# vidObj object calls read 
         success, image = camera.read()
-		# function extract frames         
+	# function extract frames         
         frame = cv2.resize(image, (640, 480))  
- 		# encode frames        
+ 	# encode frames        
         encoded, buffer = cv2.imencode('.jpg', frame)
         jpg_as_text = base64.b64encode(buffer)
-		# Count the frames with frame-count 
+	# Count the frames with frame-count 
         count +=1
         print(count)
-        channel.basic_publish(exchange='', routing_key='hello', body=jpg_as_text)
-        print(" [x] Sent Frame %d to 'hello' queue" % count)
+        channel.basic_publish(exchange='', routing_key='webcam1', body=jpg_as_text)
+        print(" [x] Sent Frame %d to 'webcam1' queue" % count)
 connection.close()
 
 # Function to extract frames 
